80fc1420fca70f02a6427c864f2f16ff7893fd4f
vaibhu4coding <vaibhu4coding@gmail.com>
2023-10-27 20:18:03 +0530

    finale

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,5 +2,5 @@ const express = require('express')
 const showUsersOnParticularDate = require('../controllers/userController')
 const authMiddleware = require('../utils/auth')
 const router = express.Router()
-router.get("/user-cnt", authMiddleware,showUsersOnParticularDate)
+router.post("/user-cnt",authMiddleware,showUsersOnParticularDate)
 module.exports = router
